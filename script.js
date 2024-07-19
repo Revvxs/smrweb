ded4a63d4115fd56133beab4daa6a9d136c8d02f
Revvxs <151439160+Revvxs@users.noreply.github.com>
2024-07-19 01:04:05 -0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const qnas = document.querySelectorAll('#qna h2');
-
-qnas.forEach((qa) => {
-  qa.addEventListener('click', () => {
-    const answer = qa.nextElementSibling;
-    answer.style.display = (answer.style.display === 'none') ? 'block' : 'none';
-  });
-});
