3cd16cd32c38a428cbd6b86c25954651529c0c45
Revvxs <151439160+Revvxs@users.noreply.github.com>
2024-07-18 22:19:00 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
- <script>
-    const sections = document.querySelectorAll('section');
-    const navLinks = document.querySelectorAll('nav ul li a');
-
-    navLinks.forEach((link) => {
-      link.addEventListener('click', (e) => {
-        e.preventDefault();
-        const targetId = e.target.getAttribute('href');
-        sections.forEach((section) => {
-          if (section.id !== targetId) {
-            section.style.display = 'none';
-          } else {
-            section.style.display = 'block';
-          }
-        });
+document.addEventListener('DOMContentLoaded', () => {
+  const navLinks = document.querySelectorAll('nav ul li a');
+  navLinks.forEach((link) => {
+    link.addEventListener('click', (e) => {
+      e.preventDefault();
+      const targetId = e.target.getAttribute('href');
+      sections.forEach((section) => {
+        if (section.id !== targetId) {
+          section.classList.remove('active');
+        }
+        if (section.id === targetId) {
+          section.classList.add('active');
+        }
       });
     });
+  });
+});
